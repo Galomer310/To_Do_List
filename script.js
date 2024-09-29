3965944fb07285426f12001761fc4cfe74474cfa
Gal Omer <galomer6708@gmail.com>
2024-09-29 13:25:31 +0300

    final commit

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
-// the ul element that will contain the list items
 const toDoList = document.getElementById("todo-list");
-// the button add 
 const addingBtn = document.getElementById("addBtn");
-//  the input filed where you write your text 
 const newTaskInput = document.getElementById("newTask");
+const clearTesk = document.getElementById("clear-tesk");
+clearTesk.addEventListener("click", function(event) {
+  toDoList.innerHTML = '';
+});
 
 addingBtn.addEventListener("click", function(event) {
-  // Prevent form submission
   event.preventDefault();
-  // Get the task text from the input field
   const taskText = newTaskInput.value.trim();
   
-  // Only add the task if there's content in the input
   if (taskText) {
-  // Create new list item
-    const teskInput = document.createElement('li');
+    const newTeskList = document.createElement('li');
     
     const deleteBtn = document.createElement('span');
     deleteBtn.classList.add('delete');
     deleteBtn.innerHTML = '❌';
     deleteBtn.addEventListener("click", function() {
-      teskInput.remove();
+      newTeskList.remove();
     });
 
     const checkbox = document.createElement('input');
@@ -28,24 +25,21 @@ addingBtn.addEventListener("click", function(event) {
     checkbox.classList.add('tesk-check');
     checkbox.addEventListener('change', function() {
       if (checkbox.checked) {
-        taskSpan.classList.add('completed');  // Strike-through if checked
+        taskSpan.classList.add('completed');  
       } else {
-        taskSpan.classList.remove('completed');  // Remove strike-through if unchecked
+        taskSpan.classList.remove('completed');  
       }
     });
   
-       // Create the task text span
     const taskSpan = document.createElement('span');
     taskSpan.classList.add('task');
     taskSpan.textContent = taskText;
 
-    teskInput.appendChild(deleteBtn);
-    teskInput.appendChild(checkbox);
-    teskInput.appendChild(taskSpan);
-    // Append the new task to the list
-    toDoList.appendChild(teskInput);
+    newTeskList.appendChild(deleteBtn);
+    newTeskList.appendChild(checkbox);
+    newTeskList.appendChild(taskSpan);
+    toDoList.appendChild(newTeskList);
     
-    // Clear the input field after adding the task
     newTaskInput.value = '';
   } else {
     alert("Please enter a task.");
